a31d21249bca8f00a067ef5d7eed51a234f09c11
Sebastian Guerrero <wesdell17@gmail.com>
2026-06-05 10:01:52 -0500

    Markup Navbar component and add type declaration for global css

---
+++ client/src/app/DashboardWrapper.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "@/components/Navbar";
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-9000">
+      sssssiii
+      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default DashboardWrapper;
