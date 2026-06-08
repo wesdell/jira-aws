2323287dacab30709141f5228fa5002a1c6a866d
Sebastian Guerrero <wesdell17@gmail.com>
2026-06-08 10:25:33 -0500

    Configure Redux provider add env variables example file and dynamic style to toggle between dark and light mode

--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -1,15 +1,41 @@
+"use client"
+
+import { useEffect } from "react";
+
+import StoreProvider, { useAppSelector } from "./redux";
+
 import { Navbar, Sidebar } from "@/components";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSideBarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+
   return (
-    <div className="flex min-h-screen w-full bg-gray-50 text-gray-9000">
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSideBarCollapsed ? "" : "md:pl-64"}`}>
         <Navbar />
         {children}
       </main>
     </div>
   )
-}
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
 
 export default DashboardWrapper;
