3be436b924d7be0a2c07682cf674f63da02df5e6
Sebastian Guerrero <wesdell17@gmail.com>
2026-06-09 09:50:49 -0500

    Markup sidebar links and making siderbar collapsable

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,20 +1,37 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-import { LockIcon } from "lucide-react";
+import { Briefcase, Home, LockIcon, Search, Settings, User, Users, X } from "lucide-react";
+
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
+import { SidebarLinkProps } from "@/types";
 
 export const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSideBarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+
   return (
     <div className={`fixed flex flex-col justify-between shadow-xl
-    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
-    w-64`}>
+    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white ${isSideBarCollapsed ? "w-0 hidden" : "w-64"}`}>
       <div className="flex h-full w-full flex-col justify-start">
-
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="text-xl font-bold text-gray-800 dark:text-white">
             WEDLIST
           </div>
+          {isSideBarCollapsed ? null : (
+            <button
+              className="py-3"
+              onClick={() => {
+                dispatch(setIsSidebarCollapsed(!isSideBarCollapsed));
+              }}
+            >
+              <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+            </button>
+          )}
         </div>
 
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
@@ -34,7 +51,39 @@ export const Sidebar = () => {
             </div>
           </div>
         </div>
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" />
+          <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
+          <SidebarLink icon={Search} label="Search" href="/search" />
+          <SidebarLink icon={Settings} label="Settings" href="/settings" />
+          <SidebarLink icon={User} label="Users" href="/users" />
+          <SidebarLink icon={Users} label="Teams" href="/teams" />
+        </nav>
       </div>
     </div>
+  );
+};
+
+const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+          } justify-start px-8 py-3`}
+      >
+        {isActive && (
+          <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
+        )}
+
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   );
 };
---
+++ client/src/types/components.ts
@@ -0,0 +1,7 @@
+import { LucideIcon } from "lucide-react";
+
+export interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+}
