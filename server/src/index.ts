6851fa94124749b09bbc66a0364fb910243ff7c0
Sebastian Guerrero <wesdell17@gmail.com>
2026-06-10 11:01:18 -0500

    Create server with a healthy endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running in: http://localhost:${port}`);
+});
