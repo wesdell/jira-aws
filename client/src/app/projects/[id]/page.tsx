68ddd419a354507b41811be53db49a3354704adb
Sebastian Guerrero <wesdell17@gmail.com>
2026-06-30 09:59:40 -0500

    Change project view depending on view status

---
+++ client/src/app/projects/[id]/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
+import Board from "../BoardView";
+import List from "../ListView";
+import ProjectHeader from "../ProjectHeader";
+
+type Props = {
+  params: { id: string };
+};
+
+const Project = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+  return (
+    <div>
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+    </div>
+  );
+};
+
+export default Project;
