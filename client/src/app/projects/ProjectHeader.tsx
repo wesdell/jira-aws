f2d3f4bb6494f18ec916cd098660829aba96c8ed
Sebastian Guerrero <wesdell17@gmail.com>
2026-07-01 10:09:01 -0500

    New Modal components to create a project

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -11,6 +11,7 @@ import {
 } from "lucide-react";
 
 import { Header } from "@/components";
+import ModalNewProject from "./ModalNewProject";
 
 type Props = {
   activeTab: string;
@@ -22,6 +23,10 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
 
   return (
     <div className="px-4 xl:px-6">
+      <ModalNewProject
+        isOpen={isModalNewProjectOpen}
+        onClose={() => setIsModalNewProjectOpen(false)}
+      />
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
         <Header
           name="Product Design Development"
---
+++ client/src/components/Modal.tsx
@@ -0,0 +1,39 @@
+import ReactDOM from "react-dom";
+
+import { X } from "lucide-react";
+
+import { Header } from "@/components";
+
+type Props = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  name: string;
+};
+
+export const Modal = ({ children, isOpen, onClose, name }: Props) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
+    <div className="fixed inset-0 z-50 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600 bg-opacity-50 p-4">
+      <div className="w-full max-w-2xl rounded-lg bg-white p-4 shadow-lg dark:bg-dark-secondary">
+        <Header
+          name={name}
+          buttonComponent={
+            <button
+              className="flex h-7 w-7 items-center justify-center rounded-full bg-blue-primary text-white hover:bg-blue-600"
+              onClick={onClose}
+            >
+              <X size={18} />
+            </button>
+          }
+          isSmallText
+        />
+        {children}
+      </div>
+    </div>,
+    document.body,
+  );
+};
