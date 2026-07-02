4b4b98a79fac74cccf89ccfeec2c0310ed506e54
Sebastian Guerrero <wesdell17@gmail.com>
2026-07-02 10:28:44 -0500

    Add Home Page dashboard

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
+import HomePage from "./home/page";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello, World!
-    </main>
+    <HomePage />
   );
 }
